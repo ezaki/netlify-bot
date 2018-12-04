39a058c737dd12e85fa08018db4819888abd06a0
ezaki <takato.ezaki@gmail.com>
2018-12-05 00:10:42 +0900

    add root

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ const fastify = require('fastify')({
   logger: true
 })
 
+fastify.get('/', (req, res) => {
+  res.send('ok')
+})
+
 fastify.post('/deploy', (req, res) => {
   console.log(req.body)
+  res.send('ok')
 })
 
 const port = process.env.PORT || 3000
