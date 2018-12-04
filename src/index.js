e2b669d2c5c667d6e6843bfef425a2b7ed64c6c6
ezaki <takato.ezaki@gmail.com>
2018-12-05 00:43:03 +0900

    add hook

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const https = require('https')
 const app = require('express')()
 const parser = require('body-parser')
 
@@ -8,8 +9,41 @@ app.get('/', (req, res) => {
 })
 
 app.post('/deploy', (req, res) => {
-  console.log(req.body)
-  res.send('ok')
+  if (req.body.token !== process.env.TOKEN) {
+    return res.send('Invalid token.')
+  }
+
+  const hostname = process.env.BUILD_WEBHOOK.match(/^https?:\/\/([^/]+)\/.*$/)[1]
+  const path = process.env.BUILD_WEBHOOK.match(/^https?:\/\/(?:[^/]+)(\/.*)$/)[1]
+
+  console.log(`Hostname: ${hostname}`)
+  console.log(`Path: ${path}`)
+
+  const hookReq = https.request({
+    hostname,
+    path,
+    method: 'POST'
+  }, (res) => {
+    const data = []
+    res.on('data', (c) => {
+      data.push(c)
+    })
+    res.on('end', () => {
+      const buf = Buffer.concat(data)
+      console.log(buf.toString('base64'))
+    })
+    res.on('error', (e) => {
+      console.error(e)
+    })
+  })
+
+  hookReq.on('error', (e) => {
+    console.error(e)
+  })
+  hookReq.write(JSON.stringify({}))
+  hookReq.end()
+
+  res.send('OK. Deploy start.')
 })
 
 const port = process.env.PORT || 3000
